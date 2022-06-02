4809574e3c376559362301d931f84858e245509f
David Crescence <david.crescence@orange.com>
2022-06-02 17:46:38 +0200

    Updating test file with according sonar message rule test.

--- src/android-plugin/src/test/files/sobriety/HighFrameRateCheck.java
+++ src/android-plugin/src/test/files/sobriety/HighFrameRateCheck.java
@@ -7,10 +7,9 @@ class Surface {
     private static final int CHANGE_FRAME_RATE_ALWAYS = 1;
     private static final int CHANGE_FRAME_RATE_ONLY_IF_SEAMLESS = 0;
 
-    public void  test() {
+    public void test() {
         Surface surface = new Surface();
-        surface.setFrameRate(120.0f,0); // Noncompliant
-//        surface.setFrameRate(90.0f,FRAME_RATE_COMPATIBILITY_FIXED_SOURCE);
+        surface.setFrameRate(0.0f,0); // Noncompliant {{Not overriding setFrameRate default behavior is recommanded to avoid higher battery usage.}}
     }
 
     public void	setFrameRate(float frameRate, int compatibility) {
