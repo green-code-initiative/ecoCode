da63c7580088f8bdb7f12b21d05a8b1f69203851
bantra7 <renault.jbapt@gmail.com>
2022-06-03 10:56:45 +0200

    Add rule D7 Python - Add new tests

--- src/python-plugin/src/test/java/fr/cnumr/python/checks/AvoidGettersAndSettersTest.java
+++ src/python-plugin/src/test/java/fr/cnumr/python/checks/AvoidGettersAndSettersTest.java
@@ -7,7 +7,6 @@ public class AvoidGettersAndSettersTest {
     
     @Test
     public void test() {
-        PythonCheckVerifier.verify("src/test/resources/checks/avoidGettersAndSettersRaiseIssue.py", new AvoidGettersAndSetters());
-        PythonCheckVerifier.verifyNoIssue("src/test/resources/checks/avoidGettersAndSetters.py", new AvoidGettersAndSetters());
+        PythonCheckVerifier.verify("src/test/resources/checks/avoidGettersAndSetters.py", new AvoidGettersAndSetters());
     }
 }
