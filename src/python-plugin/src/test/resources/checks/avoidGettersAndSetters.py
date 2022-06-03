17a5e29231fe44e991a06bcb5a93cf56deb89726
bantra7 <renault.jbapt@gmail.com>
2022-06-03 13:17:37 +0200

    D7-PYTHON Ajout de la vérification des setters

--- src/python-plugin/src/test/resources/checks/avoidGettersAndSetters.py
+++ src/python-plugin/src/test/resources/checks/avoidGettersAndSetters.py
@@ -6,15 +6,18 @@ class Client():
         self.age = age
         self.weight = weight
 
+    def set_age(self, age): # Noncompliant {{Avoid the use of getters and setters}}
+        self.age = age
+
+    def set_age(self, age2): # Noncompliant {{Avoid the use of getters and setters}}
+        self.age = age2
+
     def get_age_in_five_years(self):
         a = Client()
         return a.age
 
     def get_age(self): # Noncompliant {{Avoid the use of getters and setters}}
         return self.age
-
-    def set_age(self, age):
-        self.age = age
 
     def is_major(self):
         return self.age >= 18
