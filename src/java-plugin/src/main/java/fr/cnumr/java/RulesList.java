3a9a8bdc88b120d01f80c64b93aa52e27b3ab794
Romain DUCASSE <rducasse@aubay.com>
2022-06-03 11:47:22 +0200

    Add rule for string buffer/builder initialization #GRSP0032-JAVA

--- src/java-plugin/src/main/java/fr/cnumr/java/RulesList.java
+++ src/java-plugin/src/main/java/fr/cnumr/java/RulesList.java
@@ -46,7 +46,8 @@ public final class RulesList {
                 AvoidSQLRequestInLoop.class,
                 AvoidFullSQLRequest.class,
                 UseCorrectForLoop.class,
-                UnnecessarilyAssignValuesToVariables.class
+                UnnecessarilyAssignValuesToVariables.class,
+                InitializeBufferWithAppropriateSize.class
         ));
     }
 
