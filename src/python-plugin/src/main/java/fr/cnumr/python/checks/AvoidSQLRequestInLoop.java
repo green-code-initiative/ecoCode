c86a82eced6a27b2133cf7bfd327086da64446a5
Vincent Cagnard <vincent.cagnard@davidson.fr>
2022-06-22 18:54:57 +0200

    Updating rules test

--- src/python-plugin/src/main/java/fr/cnumr/python/checks/AvoidSQLRequestInLoop.java
+++ src/python-plugin/src/main/java/fr/cnumr/python/checks/AvoidSQLRequestInLoop.java
@@ -3,17 +3,20 @@ package fr.cnumr.python.checks;
 import org.sonar.check.Priority;
 import org.sonar.check.Rule;
 import org.sonar.plugins.python.api.PythonSubscriptionCheck;
+import org.sonar.plugins.python.api.SubscriptionContext;
 import org.sonar.plugins.python.api.tree.*;
 
 import java.util.*;
 import java.lang.*;
 
 @Rule(
-        key = "S64",
-        name = "Developpement",
-        description = AvoidSQLRequestInLoop.MESSAGERULE,
-        priority = Priority.MINOR,
-        tags = {"bug"})
+    key = "S64",
+    name = "Developpement",
+    description = AvoidSQLRequestInLoop.MESSAGERULE,
+    priority = Priority.MINOR,
+    tags = {"bug"}
+)
+
 public class AvoidSQLRequestInLoop extends PythonSubscriptionCheck {
 
     public static final String MESSAGERULE = "Avoid perform an SQL query inside a loop";
@@ -21,63 +24,51 @@ public class AvoidSQLRequestInLoop extends PythonSubscriptionCheck {
 
     @Override
     public void initialize(Context context) {
-
         context.registerSyntaxNodeConsumer(Tree.Kind.FOR_STMT, ctx -> {
-            ForStatement forStatement = (ForStatement) ctx.syntaxNode();
-            StatementList list = (StatementList) forStatement.body();
-            for (Statement a : list.statements()) {
-                if (a.getKind().equals(Tree.Kind.EXPRESSION_STMT)) {
-                    ExpressionStatement expression = (ExpressionStatement) a;
-                    for (Expression i : expression.expressions()) {
-                        CallExpression call = (CallExpression) i;
-                        for (Tree ele : call.callee().children()) {
-                            if (ele.getKind().equals(Tree.Kind.NAME)) {
-                                Name name = (Name) ele;
-                                if (name.name().equals("execute")) {
-                                    for (Argument argument : call.arguments()) {
-                                        StringLiteral string = (StringLiteral) argument.children().get(0);
-                                        if (string.stringElements().get(0).value().toUpperCase().contains("SELECT")) {
-                                                ctx.addIssue(call, MESSAGERULE);
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+            System.out.println("FOR_STMT");
+            visitLoopNode(((ForStatement) ctx.syntaxNode()).body(), ctx);
         });
         context.registerSyntaxNodeConsumer(Tree.Kind.WHILE_STMT, ctx -> {
-            WhileStatement whileStatement = (WhileStatement) ctx.syntaxNode();
-            StatementList list = (StatementList) whileStatement.body();
-            for (Statement a : list.statements()) {
-                if (a.getKind().equals(Tree.Kind.EXPRESSION_STMT)) {
-                    ExpressionStatement expression = (ExpressionStatement) a;
-                    for (Expression i : expression.expressions()) {
-                        CallExpression call = (CallExpression) i;
-                        for (Tree ele : call.callee().children()) {
-                            if (ele.getKind().equals(Tree.Kind.NAME)) {
-                                Name name = (Name) ele;
-                                if (name.name().equals("execute")) {    
-                                    for (Argument argument : call.arguments()) {    
-                                            if (argument instanceof BinaryExpression){
-                                                BinaryExpression binOp = (BinaryExpression) ele;
-                                                System.out.println(binOp.leftOperand());
-                                                ctx.addIssue(call, MESSAGERULE);
-                                            }else {
-                                                if (argument.children().get(0).toString().toUpperCase().contains("SELECT")) {
-                                                    ctx.addIssue(call, MESSAGERULE);
-                                                }       
-                                            }
-                                    }
-                                }
-                            }
-                        }
-                    }
-
-                }
-            }
-
+            System.out.println("WHILE_STMT");
+            visitLoopNode(((WhileStatement) ctx.syntaxNode()).body(), ctx);
         });
     }
+
+    private void visitLoopNode(StatementList list, SubscriptionContext ctx) {
+        for (Statement a: list.statements()) {
+            if (a.getKind().equals(Tree.Kind.EXPRESSION_STMT)) {
+                ExpressionStatement expression = (ExpressionStatement) a;
+                for (Expression i: expression.expressions()) {
+                    if (i.is(Tree.Kind.CALL_EXPR))
+                        visitCallExpressionNode((CallExpression) i, ctx);
+                }
+            }
+        }
+    }
+
+    private void visitCallExpressionNode(CallExpression ce, SubscriptionContext ctx) {
+        for (Tree ele : ce.callee().children()) {
+            if (ele.getKind().equals(Tree.Kind.NAME)) {
+                Name name = (Name) ele;
+                if (name.name().equals("execute")) {    
+                    for (Argument a: ce.arguments()) {
+                        if (checkLitteralInTree((Tree) a))
+                            ctx.addIssue(ce, MESSAGERULE);
+                    }
+                }
+            }
+        }
+    }
+
+    private boolean checkLitteralInTree(Tree t) {
+        for (Tree tc : t.children()) {
+            if (tc.is(Tree.Kind.STRING_LITERAL)) {
+                if (((StringLiteral) tc).trimmedQuotesValue().toUpperCase().contains("SELECT"))
+                    return true;
+            }
+            return checkLitteralInTree(tc);
+        }
+        return false;
+    }
+
 }
