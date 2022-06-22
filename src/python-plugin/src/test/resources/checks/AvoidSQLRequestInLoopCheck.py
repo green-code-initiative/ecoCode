fc1cfbd617ff135e853573f688220961d0f9e487
Vincent Cagnard <vincent.cagnard@davidson.fr>
2022-06-22 18:49:26 +0200

    Insure that python code is functionnal (we can manualy test it), doing homogeneous tests

--- src/python-plugin/src/test/resources/checks/AvoidSQLRequestInLoopCheck.py
+++ src/python-plugin/src/test/resources/checks/AvoidSQLRequestInLoopCheck.py
@@ -2,61 +2,82 @@ import mysql.connector
 
 
 class AvoidSQLRequestInLoopCheck:
+    maxrows = 20
+
     def testWithNoLoop(self):
         try :
             db = mysql.connector.connect(option_files='my.conf', use_pure=True)
-            cursor=db.cursor()
-            query = "SELECT * FROM users"
-            cursor.execute(query)
-            with row in cursor:
-                print(row.id)
+            cursor = db.cursor(dictionary=True)
+
+            cursor.execute('SELECT id, name FROM users LIMIT %(limit)s', {'limit': self.maxrows})
+            for row in cursor.fetchall():
+                print("{}: {}".format(row['id'], row['name']))
+
             cursor.close()
-        except :
-            print("Got an exception")
+            db.close()
+        except mysql.connector.Error as err:
+            print("Got an exception: {}".format(err))
             db.close()
 
-    def testWithForLoop():
+    def testWithForLoop(self):
         try:
             db = mysql.connector.connect(option_files='my.conf', use_pure=True)
-            cursor=db.cursor()
-            for i in range(0,20):
-                query+=str(i)
-                cursor.execute("SELECT * from users") #Noncompliant
-            with row in cursor:
-                print(row.name)
+            cursor = db.cursor(dictionary=True)
+
+            for i in range(0, self.maxrows):
+                cursor.execute("SELECT id, name FROM users WHERE id = %(id)s", {'id': i+1}) #Noncompliant
+                for row in cursor.fetchall():
+                    print("{}: {}".format(row['id'], row['name']))
+
             cursor.close()
-        except :
-            print("Got an exception")
+            db.close()
+        except mysql.connector.Error as err:
+            print("Got an exception: {}".format(err))
             db.close()
 
-    def testWithWhileLoop():
-          try:
-              db = mysql.connector.connect(option_files='my.conf', use_pure=True)
-              i = 0
-              while i<20:
-                  cursor=db.cursor()
-                  cursor.execute("SELECT * FROM users where id = " + str(i)) #Noncompliant
-                  i+=1
-              with row in cursor:
-                  print(row.name)
-              cursor.close()
-          except :
-              print("Got an exception")
-              db.close()
-
-    def testWithWhileLoopUpdate():
+    def testWithWhileLoop(self):
         try:
             db = mysql.connector.connect(option_files='my.conf', use_pure=True)
+            cursor = db.cursor(dictionary=True)
+
             i = 0
-            while i<20:
-                cursor=db.cursor()
-                cursor.execute("UPDATE users set id="+str(i))
-                i+=1
-            with row in cursor:
-                print(row.name)
+            while i < self.maxrows:
+                cursor.execute("SELECT id, name FROM users WHERE id = %(id)s", {'id': i+1}) #Noncompliant
+                for row in cursor.fetchall():
+                    print("name: {}".format(row['name']))
+                i += 1
+
             cursor.close()
-        except :
-            print("Got an exception")
+            db.close()
+        except mysql.connector.Error as err:
+            print("Got an exception: {}".format(err))
             db.close()
 
+    def testWithWhileLoopUpdate(self):
+        try:
+            db = mysql.connector.connect(option_files='my.conf', use_pure=True)
+            cursor=db.cursor()
 
+            i = 0
+            while i < self.maxrows:
+                cursor.execute('UPDATE users set name=%(name)s where id=%(id)s', {'name': "anonymous", 'id': i+1})
+                i+=1
+            db.commit()
+
+            cursor.close()
+            db.close()
+        except mysql.connector.Error as err:
+            print("Got an exception: {}".format(err))
+            db.close()
+
+if __name__ == '__main__':
+  test = AvoidSQLRequestInLoopCheck()
+
+  print("testWithNoLoop")
+  test.testWithNoLoop()
+  print("testWithForLoop")
+  test.testWithForLoop()
+  print("testWithWhileLoop")
+  test.testWithWhileLoop()
+  print("testWithWhileLoopUpdate")
+  test.testWithWhileLoopUpdate()
