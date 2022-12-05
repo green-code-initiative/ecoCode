8e1c35bda438168cd3da3b7bf7999ddd179dc5b7
David DE CARVALHO <ddecarvalho@solocal.com>
2022-12-05 21:11:52 +0100

    migration - clean android part

--- src/prepare-codenarc.bat
+++
@@ -1,9 +0,0 @@
-REM == Define CodeNarc version
-set codenarc_version=2.2.3
-
-REM == Build CodeNarc
-cd codenarc-converter/CodeNarc
-call ./gradlew build -x test
-
-REM == Deploy to local repository
-mvn -B install:install-file -Dfile=build/libs/CodeNarc-%codenarc_version%.jar -DgroupId=org.codenarc -DartifactId=CodeNarc -Dversion=%codenarc_version% -Dpackaging=jar
