f26e9b5264212f2bfe55a1120813749c282a217f
Laurent Goncalves <lgoncalves@e-citiz.com>
2022-12-13 15:12:23 +0100

    Example of failure if a new is encountered in a file before a try statement

--- java-plugin/src/test/files/OptimizeReadFileExceptionCheck.java
+++ java-plugin/src/test/files/OptimizeReadFileExceptionCheck.java
@@ -1,11 +1,20 @@
 package fr.cnumr.java.checks;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
 
 class ReadFile {
 	ReadFile(ReadFile readFile) {
 	}
+	
+	protected String getMessageFrom(final String value) {
+		final String string = new String(value);
+		return string;
+	}
+	
 	public void readPreferences(String filename) {
 		//...
 		InputStream in = null;
@@ -16,4 +25,5 @@ class ReadFile {
 		}
 		//...
 	}
+
 }
