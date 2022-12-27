66acfb2f5365cccd60623b4849dd1ea6401dbebc
David DE CARVALHO <ddecarvalho@solocal.com>
2022-12-27 23:26:49 +0100

    test releasing 2

--- tool_release_1_prepare.sh
+++ tool_release_1_prepare.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env sh
 
+# creation of 2 commits with release and next SNAPSHOT
 mvn release:prepare -B -ff -DpushChanges=false -DtagNameFormat=@{project.version}
+
+# clean temporary files
 mvn release:clean
-LAST_TAG=$(git tag --sort=-version:refname | head -n 1)
-BRANCH_NAME=$(echo "release_${LAST_TAG}")
-git checkout -b ${BRANCH_NAME}
-git push --set-upstream origin ${BRANCH_NAME}
