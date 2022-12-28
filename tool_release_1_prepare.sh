d2bd81d172391570c7fefa0126995271cea8b21e
David DE CARVALHO <ddecarvalho@solocal.com>
2022-12-28 22:44:54 +0100

    upgrade tool release

--- tool_release_1_prepare.sh
+++ tool_release_1_prepare.sh
@@ -7,7 +7,7 @@
 # creation of 2 commits with release and next SNAPSHOT
 mvn release:prepare -B -ff -DpushChanges=false -DtagNameFormat=@{project.version}
 
-sleep 2000
+sleep 2
 
 # clean temporary files
 mvn release:clean
