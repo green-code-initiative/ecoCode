a841aeff276bdb9711db826e6cdbbce46a2b51df
Oussama <oussamala72@gmail.com>
2023-01-10 15:14:37 +0100

    Add Nicolas remarks to PR #6.

--- php-plugin/src/main/java/fr/cnumr/php/checks/AvoidRelativePathCheck.java
+++ php-plugin/src/main/java/fr/cnumr/php/checks/AvoidRelativePathCheck.java
@@ -3,6 +3,7 @@ package fr.cnumr.php.checks;
 import org.sonar.check.Priority;
 import org.sonar.check.Rule;
 import org.sonar.plugins.php.api.tree.Tree;
+import org.sonar.plugins.php.api.tree.expression.ExpressionTree;
 import org.sonar.plugins.php.api.tree.expression.FunctionCallTree;
 import org.sonar.plugins.php.api.tree.expression.ParenthesisedExpressionTree;
 import org.sonar.plugins.php.api.visitors.PHPSubscriptionCheck;
@@ -19,6 +20,9 @@ import java.util.*;
 public class AvoidRelativePathCheck extends PHPSubscriptionCheck {
 
     public static final String ERROR_MESSAGE = "Avoid using relative path, prefer using absolute path";
+    private static final String REG_EXP_INClUDE_REQUIRE_TYPES = ".*include.*|.*require.*|.*require_once.*|.*include_once.*";
+    private static final String REG_EXP_FILTER = ".*__DIR__.*|.*__FILE__.*|.* . .*";
+
     @Override
     public List<Tree.Kind> nodesToVisit() {
         return Collections.singletonList(Tree.Kind.FUNCTION_CALL);
@@ -31,12 +35,29 @@ public class AvoidRelativePathCheck extends PHPSubscriptionCheck {
     }
 
     public void checkIssue(FunctionCallTree functionCallTree) {
-        if(functionCallTree.callee().toString().equals("include") || functionCallTree.callee().toString().equals("require")){
-            ParenthesisedExpressionTree expressionTree = (ParenthesisedExpressionTree) functionCallTree.
-                    callArguments().get(0).value();
-            if(!expressionTree.expression().toString().startsWith("\"/")){
-                context().newIssue(this, functionCallTree, ERROR_MESSAGE);
-            }
+        if(functionCallTree.callee().toString().matches(REG_EXP_INClUDE_REQUIRE_TYPES)){
+
+                ExpressionTree expressionTree =  functionCallTree.callArguments().get(0).value();
+                 if(expressionTree.toString().indexOf('(') != -1){
+                     parenthesisedExpressionTreeCheck(expressionTree,functionCallTree);
+                }else{
+                     expressionTreeCheck(expressionTree,functionCallTree);
+                }
         }
     }
-}
+    public void parenthesisedExpressionTreeCheck(ExpressionTree expressionTree,FunctionCallTree functionCallTree){
+        ParenthesisedExpressionTree parenthesisedExpressionTree = (ParenthesisedExpressionTree) expressionTree;
+        if(!parenthesisedExpressionTree.expression().toString().matches(REG_EXP_FILTER) && !parenthesisedExpressionTree.expression().toString().startsWith("\"/")){
+            report(functionCallTree);
+        }
+    }
+    public void expressionTreeCheck(ExpressionTree expressionTree,FunctionCallTree functionCallTree){
+        if(!expressionTree.toString().matches(REG_EXP_FILTER) && !expressionTree.toString().startsWith("\"/")){
+            report(functionCallTree);
+        }
+    }
+
+    private void report(FunctionCallTree functionCallTree) {
+        context().newIssue(this, functionCallTree, ERROR_MESSAGE);
+    }
+    }
