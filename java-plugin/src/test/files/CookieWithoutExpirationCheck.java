780e0b1ffbc6c725d3f19bce70fc7ad1f4cdfcc7
Said MOHAMEDALI <Said.MOHAMEDALI@labinnov.fr>
2023-04-05 16:06:55 +0200

    adding test file for Cookie without ExpirationCheck

---
+++ java-plugin/src/test/files/CookieWithoutExpirationCheck.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+class CustomerDataCopyCheck {
+
+	public void NOK_CookieCreation() {
+		// create objet cookie 
+		Cookie C = new Cookie("id","674684641");
+		// set the validity
+		C.setMaxAge(24*3600);
+	}
+
+
+	
+}
