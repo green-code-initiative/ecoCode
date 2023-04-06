7f050a2ee0700f662b6731bc09802e6f4f30221b
Eliott Lavier <67601109+eliottlv@users.noreply.github.com>
2023-04-06 09:59:30 +0200

    [ISSUE 113] Use unoptimized vector images

--- python-plugin/src/main/java/fr/greencodeinitiative/python/PythonRuleRepository.java
+++ python-plugin/src/main/java/fr/greencodeinitiative/python/PythonRuleRepository.java
@@ -29,12 +29,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import fr.greencodeinitiative.python.checks.AvoidFullSQLRequest;
-import fr.greencodeinitiative.python.checks.AvoidGettersAndSetters;
-import fr.greencodeinitiative.python.checks.AvoidGlobalVariableInFunctionCheck;
-import fr.greencodeinitiative.python.checks.AvoidSQLRequestInLoop;
-import fr.greencodeinitiative.python.checks.AvoidTryCatchFinallyCheck;
-import fr.greencodeinitiative.python.checks.NoFunctionCallWhenDeclaringForLoop;
+import fr.greencodeinitiative.python.checks.*;
 import org.apache.commons.lang.StringUtils;
 import org.sonar.api.rules.RuleType;
 import org.sonar.api.server.rule.RulesDefinition;
@@ -93,6 +88,7 @@ public class PythonRuleRepository implements RulesDefinition, PythonCustomRuleRe
             AvoidGlobalVariableInFunctionCheck.class,
             AvoidSQLRequestInLoop.class,
             AvoidTryCatchFinallyCheck.class,
+            AvoidUnoptimizedVectorImagesCheck.class,
             NoFunctionCallWhenDeclaringForLoop.class,
             AvoidFullSQLRequest.class
     );
