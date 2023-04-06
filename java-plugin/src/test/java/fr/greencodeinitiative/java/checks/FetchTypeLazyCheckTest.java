19f43fea5d4ce1ff45624c51c0cf5f081df4bae1
mohamed qelai <mohamed.qelai@gmail.com>
2023-04-06 13:51:54 +0200

    modify class visibility

--- java-plugin/src/test/java/fr/greencodeinitiative/java/checks/FetchTypeLazyCheckTest.java
+++ java-plugin/src/test/java/fr/greencodeinitiative/java/checks/FetchTypeLazyCheckTest.java
@@ -3,7 +3,7 @@ package fr.greencodeinitiative.java.checks;
 import org.junit.jupiter.api.Test;
 import org.sonar.java.checks.verifier.CheckVerifier;
 
-public class FetchTypeLazyCheckTest {
+class FetchTypeLazyCheckTest {
 
     @Test
     void testNoIssues() {
