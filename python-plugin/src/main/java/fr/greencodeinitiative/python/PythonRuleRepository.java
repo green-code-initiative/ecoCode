f6eb6b4fe844ae60044f2f47af26cae85f731cea
Jean-Baptiste GINGUENE <jbginguene@solocal.com>
2023-04-06 08:32:24 +0200

    EC66-PYTHON - Avoiding the use of quotation marks

--- python-plugin/src/main/java/fr/greencodeinitiative/python/PythonRuleRepository.java
+++ python-plugin/src/main/java/fr/greencodeinitiative/python/PythonRuleRepository.java
@@ -29,12 +29,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import fr.greencodeinitiative.python.checks.AvoidFullSQLRequest;
-import fr.greencodeinitiative.python.checks.AvoidGettersAndSetters;
-import fr.greencodeinitiative.python.checks.AvoidGlobalVariableInFunctionCheck;
-import fr.greencodeinitiative.python.checks.AvoidSQLRequestInLoop;
-import fr.greencodeinitiative.python.checks.AvoidTryCatchFinallyCheck;
-import fr.greencodeinitiative.python.checks.NoFunctionCallWhenDeclaringForLoop;
+import fr.greencodeinitiative.python.checks.*;
 import org.apache.commons.lang.StringUtils;
 import org.sonar.api.server.rule.RulesDefinition;
 import org.sonar.api.server.rule.RulesDefinitionAnnotationLoader;
@@ -87,6 +82,7 @@ public class PythonRuleRepository implements RulesDefinition, PythonCustomRuleRe
   @Override
   public List<Class> checkClasses() {
     return Arrays.asList(
+            AvoidDoubleQuoteCheck.class,
             AvoidGettersAndSetters.class,
             AvoidGlobalVariableInFunctionCheck.class,
             AvoidSQLRequestInLoop.class,
---
+++ python-plugin/src/main/java/fr/greencodeinitiative/python/checks/AvoidDoubleQuoteCheck.java
@@ -0,0 +1,31 @@
+package fr.greencodeinitiative.python.checks;
+
+import org.sonar.check.Priority;
+import org.sonar.check.Rule;
+import org.sonar.plugins.python.api.PythonSubscriptionCheck;
+import org.sonar.plugins.python.api.SubscriptionContext;
+import org.sonar.plugins.python.api.tree.StringLiteral;
+import org.sonar.plugins.python.api.tree.Tree;
+
+@Rule(
+        key = AvoidDoubleQuoteCheck.RULE_KEY,
+        name = AvoidDoubleQuoteCheck.MESSAGE_RULE,
+        description = AvoidDoubleQuoteCheck.MESSAGE_RULE,
+        priority = Priority.MINOR,
+        tags = {"bug", "eco-design", "ecocode"})
+public class AvoidDoubleQuoteCheck extends PythonSubscriptionCheck {
+    public static final String RULE_KEY = "EC66";
+    public static final String MESSAGE_RULE = "Avoid using quotation mark (\"), prefer using simple quote (')";
+    @Override
+    public void initialize(Context context) {
+        context.registerSyntaxNodeConsumer(Tree.Kind.STRING_LITERAL, this::visitNodeString);
+    }
+
+    private void visitNodeString(SubscriptionContext subscriptionContext) {
+        StringLiteral stringLiteral = (StringLiteral) subscriptionContext.syntaxNode();
+
+        if (!stringLiteral.stringElements().isEmpty() && stringLiteral.stringElements().get(0).value().startsWith("\"")){
+            subscriptionContext.addIssue(stringLiteral, MESSAGE_RULE);
+        }
+    }
+}
