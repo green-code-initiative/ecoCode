d7b8d0e4939a2667d043f3e27fa9a7bb9213e9b9
mohamed qelai <mohamed.qelai@gmail.com>
2023-04-06 14:40:35 +0200

    add Hibernate relationships

--- java-plugin/src/main/java/fr/greencodeinitiative/java/checks/FetchTypeLazyCheck.java
+++ java-plugin/src/main/java/fr/greencodeinitiative/java/checks/FetchTypeLazyCheck.java
@@ -19,54 +19,55 @@ import static java.util.Collections.singletonList;
         name = "Developpement",
         description = AvoidFullSQLRequest.MESSAGERULE,
         priority = Priority.MINOR,
-        tags = { "bug" } )
+        tags = {"bug"})
 public class FetchTypeLazyCheck extends IssuableSubscriptionVisitor {
-    public static final    String MANY_TO_ONE = "ManyToOne";
-    public static final    String LAZY        = "LAZY";
+    public static final String MANY_TO_ONE = "ManyToOne";
+    public static final String LAZY = "LAZY";
+    public static final String ONE_TO_MANY = "OneToMany";
     protected static final String MESSAGERULE = "Use lazy fetch type instead of egger";
 
     @Override
     public List<Tree.Kind> nodesToVisit() {
-        return singletonList( Tree.Kind.ANNOTATION );
+        return singletonList(Tree.Kind.ANNOTATION);
     }
 
 
     @Override
-    public void visitNode( Tree tree ) {
-        TypeTree typeTree = (( AnnotationTreeImpl ) tree).annotationType();
-        if ( MANY_TO_ONE.equals( typeTree.symbolType().name() ) ) {
-            visitAnnotations( tree );
+    public void visitNode(Tree tree) {
+        TypeTree typeTree = ((AnnotationTreeImpl) tree).annotationType();
+        if (MANY_TO_ONE.equals(typeTree.symbolType().name()) || ONE_TO_MANY.equals(typeTree.symbolType().name()) || "ManyToMany".equals(typeTree.symbolType().name())) {
+            visitAnnotations(tree);
         }
     }
 
-    private void visitAnnotations( final Tree tree ) {
-        List<Tree> annotationListTree = (( AnnotationTreeImpl ) tree).children();
-        if ( !annotationListTree.isEmpty() && annotationListTree.size() > 1 ) {
-            ArgumentListTreeImpl argumentListTree = ( ArgumentListTreeImpl ) annotationListTree.get( 2 );
-            visitArguments( argumentListTree, tree );
+    private void visitAnnotations(final Tree tree) {
+        List<Tree> annotationListTree = ((AnnotationTreeImpl) tree).children();
+        if (!annotationListTree.isEmpty() && annotationListTree.size() > 1) {
+            ArgumentListTreeImpl argumentListTree = (ArgumentListTreeImpl) annotationListTree.get(2);
+            visitArguments(argumentListTree, tree);
         }
     }
 
-    private void visitArguments( final ArgumentListTreeImpl argumentListTree, final Tree tree ) {
+    private void visitArguments(final ArgumentListTreeImpl argumentListTree, final Tree tree) {
         List<Tree> argumentListTreeChildren = argumentListTree.getChildren();
-        if ( !argumentListTreeChildren.isEmpty() && argumentListTreeChildren.size() > 1 ) {
-            AssignmentExpressionTreeImpl assignmentExpressionTree = ( AssignmentExpressionTreeImpl ) argumentListTreeChildren.get( 1 );
-            visitAssignements( assignmentExpressionTree, tree );
+        if (!argumentListTreeChildren.isEmpty() && argumentListTreeChildren.size() > 1) {
+            AssignmentExpressionTreeImpl assignmentExpressionTree = (AssignmentExpressionTreeImpl) argumentListTreeChildren.get(1);
+            visitAssignements(assignmentExpressionTree, tree);
         }
     }
 
-    private void visitAssignements( final AssignmentExpressionTreeImpl assignmentExpressionTree, final Tree tree ) {
+    private void visitAssignements(final AssignmentExpressionTreeImpl assignmentExpressionTree, final Tree tree) {
         List<Tree> assignmentExpressionTreeChildren = assignmentExpressionTree.getChildren();
-        if ( !assignmentExpressionTreeChildren.isEmpty() && assignmentExpressionTreeChildren.size() > 1 ) {
-            MemberSelectExpressionTreeImpl memberSelectExpressionTree = ( MemberSelectExpressionTreeImpl ) assignmentExpressionTreeChildren.get( 2 );
-            checkFetchTypeManyToOne( tree, memberSelectExpressionTree );
+        if (!assignmentExpressionTreeChildren.isEmpty() && assignmentExpressionTreeChildren.size() > 1) {
+            MemberSelectExpressionTreeImpl memberSelectExpressionTree = (MemberSelectExpressionTreeImpl) assignmentExpressionTreeChildren.get(2);
+            checkFetchTypeManyToOne(tree, memberSelectExpressionTree);
         }
     }
 
-    private void checkFetchTypeManyToOne( final Tree tree, final MemberSelectExpressionTreeImpl memberSelectExpressionTree ) {
-        if ( !LAZY.equals( memberSelectExpressionTree.identifier()
-                                                     .name() ) ) {
-            reportIssue( tree, MESSAGERULE );
+    private void checkFetchTypeManyToOne(final Tree tree, final MemberSelectExpressionTreeImpl memberSelectExpressionTree) {
+        if (!LAZY.equals(memberSelectExpressionTree.identifier()
+                .name())) {
+            reportIssue(tree, MESSAGERULE);
         }
     }
 }
