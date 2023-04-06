b99273b46c31c4af4b90a35580111bc0b0a2ed06
mohamed qelai <mohamed.qelai@gmail.com>
2023-04-06 12:15:26 +0200

    improve test file

--- java-plugin/src/test/java/fr/greencodeinitiative/java/checks/FetchTypeLazyCheckTest.java
+++ java-plugin/src/test/java/fr/greencodeinitiative/java/checks/FetchTypeLazyCheckTest.java
@@ -6,11 +6,20 @@ import org.sonar.java.checks.verifier.CheckVerifier;
 public class FetchTypeLazyCheckTest {
 
     @Test
-    void test() {
+    void testNoIssues() {
         CheckVerifier.newVerifier()
-                .onFile("src/test/files/FetchTypeLazyCheck.java")
+                .onFile("src/test/files/FetchTypeLazyCheckGood.java")
                 .withCheck(new FetchTypeLazyCheck())
                 .verifyNoIssues();
+    }
+
+
+    @Test
+    void testIssues() {
+        CheckVerifier.newVerifier()
+                .onFile("src/test/files/FetchTypeLazyCheckBad.java")
+                .withCheck(new FetchTypeLazyCheck())
+                .verifyIssues();
     }
 
 }
