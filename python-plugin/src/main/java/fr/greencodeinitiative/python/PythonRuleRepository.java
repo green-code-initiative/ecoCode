644e78776906f6150782ee0b0c843b649cb338a7
jycr <jycr@users.noreply.github.com>
2023-04-13 00:47:38 +0200

    green-code-initiative/ecoCode#167 fix(rule): Uses same kind of rule across languages

--- python-plugin/src/main/java/fr/greencodeinitiative/python/PythonRuleRepository.java
+++ python-plugin/src/main/java/fr/greencodeinitiative/python/PythonRuleRepository.java
@@ -36,6 +36,7 @@ import fr.greencodeinitiative.python.checks.AvoidSQLRequestInLoop;
 import fr.greencodeinitiative.python.checks.AvoidTryCatchFinallyCheck;
 import fr.greencodeinitiative.python.checks.NoFunctionCallWhenDeclaringForLoop;
 import org.apache.commons.lang.StringUtils;
+import org.sonar.api.rules.RuleType;
 import org.sonar.api.server.rule.RulesDefinition;
 import org.sonar.api.server.rule.RulesDefinitionAnnotationLoader;
 import org.sonar.plugins.python.api.PythonCustomRuleRepository;
@@ -58,6 +59,7 @@ public class PythonRuleRepository implements RulesDefinition, PythonCustomRuleRe
     remediationCosts.put(AvoidSQLRequestInLoop.RULE_KEY, "10min");
     remediationCosts.put(AvoidFullSQLRequest.RULE_KEY, "20min");
     repository.rules().forEach(rule -> {
+      rule.setType(RuleType.CODE_SMELL);
       String debt = remediationCosts.get(rule.key());
 
       // TODO DDC : create support to use org.apache.commons.lang.StringUtils
